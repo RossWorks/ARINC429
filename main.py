e1beab6ecfac66ebce0db16066cbb43997096f00
RossWorks <rossetti.marc@gmail.com>
2022-01-03 11:40:41 +0100

    added ICD loading button + Stub for ICD load function

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def bin2logical():
     TxtSDI.insert(0,LogicalText["SDI"])
     TxtPayload.delete(0,'end')
     TxtPayload.insert(0,LogicalText["PAYLOAD"])
+
+def LoadICD():
+    pass
+
 home = tkinter.Tk()
 home.title("ARINC 429 Translator")
 
@@ -27,6 +31,8 @@ TxtArincFrame.grid(row=1,column=0,columnspan=2)
 CmdBin2Logical=ttk.Button(master=home,text="Translate to logical",
                           command=bin2logical)
 CmdBin2Logical.grid(row=2,column=0)
+CmdLoadICD=ttk.Button(master=home,text="Load ICD",command=LoadICD)
+CmdLoadICD.grid(row=2,column=1)
 
 LblLabel=ttk.Label(master=home,text="Label\t=> ")
 LblLabel.grid(row=3,column=0,sticky='w')
