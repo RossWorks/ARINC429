61e981daa3af9778500b8fb63ab583c531425b47
RossWorks <rossetti.marc@gmail.com>
2022-01-06 17:04:41 +0100

    reworks of private variable names + Introduced Extended label support + new check for ICD channels

--- ARINC429.py
+++ ARINC429.py
@@ -1,14 +1,14 @@
 class Frame:
 
-    __SSM_Table=("Failure Warning",
+    _SSM_Table=("Failure Warning",
                  "Functional Test",
                  "Not Computed Data",
                  "Normal Operation")
 
-    __LogicalFrame={}
-    __BinaryWord : str
+    _LogicalFrame : dict = {}
+    _BinaryWord   : str
 
-    def __CheckIntegrity(self,
+    def _CheckIntegrity(self,
                        Frame) -> int:
         oneCount : int = 0
         if len(Frame) != 32:
@@ -25,40 +25,42 @@ class Frame:
     def Decode(self,
                Frame,
                ICD,
-               Channel = "") -> Exception:
+               Channel : str = "") -> Exception:
         Label  : str
         Fields : list
-        FirstCheck = self.__CheckIntegrity(Frame)
+        FirstCheck = self._CheckIntegrity(Frame)
         if FirstCheck != 0:
-            self.__LogicalFrame.clear()
+            self._LogicalFrame = {}
             return Exception(FirstCheck)
         Tmpstr=Frame[24:32]
         Label = oct(int(Tmpstr[::-1],2))
-        self.__LogicalFrame["LABEL"]=Label
+        self._LogicalFrame["LABEL"]=Label
         SSM = Frame[1:3]
-        self.__LogicalFrame["SSM"]=self.__SSM_Table[int(SSM,2)]
+        self._LogicalFrame["SSM"]=self._SSM_Table[int(SSM,2)]
         SDI = Frame[22:24]
-        self.__LogicalFrame["SDI"]=SDI
+        self._LogicalFrame["SDI"]=SDI
         PAYLOAD=Frame[3:22]
-        self.__LogicalFrame["PAYLOAD"]=PAYLOAD
+        self._LogicalFrame["PAYLOAD"]=PAYLOAD
         if not ICD.Valid:
             return Exception(0)
-        if Channel == "":
+        if Channel not in ICD._ChannelList:
             return Exception(6)
         Key = Channel + ';' + Label[2::]
         if not ICD.FindKey(Key):
             return Exception(7)
-        Fields = ICD.__Content[Key]
-        print("ICD extracted")
+        Fields = ICD._Content[Key]
+        if Fields[0].SDI == 'XX':
+            self._AttachSDI()
         return Exception(0)
 
     def GetLogicalData(self) -> dict:
-        return self.__LogicalFrame
+        return self._LogicalFrame
 
-    def __AttachSDI(self) -> str:
-        pass
+    def _AttachSDI(self):
+        self._LogicalFrame["PAYLOAD"] += self._LogicalFrame["SDI"]
+        self._LogicalFrame["SDI"] = "Extended label"
 
-    def __ParsePayload(self) -> dict:
+    def _ParsePayload(self):
         pass
 
 class ICD:
@@ -81,15 +83,15 @@ class ICD:
             self.LSB      = LSB
             self.SDI      = SDI
 
-    __ChannelList = []
-    __Content     = {}
+    _ChannelList = []
+    _Content     = {}
     Valid  : bool = False
 
     def __init__(self) -> None:
         pass
 
     def GetChannelList(self) -> list:
-        return self.__ChannelList 
+        return self._ChannelList 
 
     def Load(self,
              FileDir : str) -> Exception:
@@ -108,26 +110,30 @@ class ICD:
             Channel = TmpLine[1]
             Label   = TmpLine[2]
             Key     = Channel + ';' + Label
-            if Channel not in self.__ChannelList:
-                self.__ChannelList.append(Channel)
+            if Channel not in self._ChannelList:
+                self._ChannelList.append(Channel)
             TmpField = self.DataField(Name     = TmpLine[0],
                                       Encoding = TmpLine[3],
                                       MSB      = TmpLine[4],
                                       LSB      = TmpLine[5],
                                       SDI      = TmpLine[6])
-            if Key not in self.__Content:
-                self.__Content[Key] = list()
-            self.__Content[Key].append(TmpField)
+            if Key not in self._Content:
+                self._Content[Key] = list()
+            self._Content[Key].append(TmpField)
         ICDfile.close()
         self.Valid = True
         return Exception(0)
 
     def FindKey(self,
                 Key = "") -> bool:
-        if Key in self.Content:
+        if Key in self._Content:
             return True
         else:
             return False
+    
+    def ExtractKey(self,
+                   Key : str) -> list:
+        return self._Content[Key]
 
 class Exception:
     title   : str
